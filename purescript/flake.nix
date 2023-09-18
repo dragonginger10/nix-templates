419dd48b47e77038e60d35e7f241587df2d8dbea
JP <dragonginger10@gmail.com>
2023-09-18 18:13:13 -0500

    updated all to 23.05

--- purescript/flake.nix
+++ purescript/flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix-flake-based Purescript development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
     flake-utils.url = "github:numtide/flake-utils";
     easy-purescript-nix = {
       url = "github:justinwoo/easy-purescript-nix";
