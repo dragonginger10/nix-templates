588288965e148dd4346a11164ec5d2a47b923f15
JP <dragonginger10@gmail.com>
2024-01-04 09:21:52 -0600

    removing flake utils

--- flake.nix
+++ flake.nix
@@ -4,196 +4,57 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    # flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, nixpkgs }: let
+    systems = ["x86_64-linux"];
+    lib = import ./lib {inherit systems; lib = nixpkgs.lib;};
+    inherit (lib.forAllSystems (pkgs: pkgs)) writeScriptBin mkShell;
+    exec = lib.forAllSystems (pkgs: pkg: "${pkgs.${pkg}}/bin/${pkg}");
+
+    dvt = writeScriptBin "dvt" ''
+      if [ -z $1 ]; then
+        echo "no template specified"
+        exit 1
+      fi
+
+      TEMPLATE=$1
+
+      ${exec "nix"} \
+        --experimental-features 'nix-command flakes' \
+        flake init \
+        --template \
+        "github:dragonginger10/nix-templates#''${TEMPLATE}"
+    '';
+
+    update = writeScriptBin "update" ''
+      for dir in `ls -d */`; do # Iterate through all the templates
+        (
+          cd $dir
+          ${exec "nix"} flake update # Update flake.lock
+          ${exec "direnv"} reload    # Make sure things work after the update
+        )
+      done
+    '';
+  in
     {
-      templates = rec {
-        clojure = {
-          path = ./clojure;
-          description = "Clojure development environment";
-        };
+      # templates = import ./templates.nix;
+      formatter = lib.forAllSystems (pkgs: pkgs.alejandra);
 
-        cue = {
-          path = ./cue;
-          description = "Cue development environment";
-        };
+      devShells = lib.forAllSystems (pkgs: 
+        pkgs.mkShell {
+          packages = with pkgs; [
+          nil
+          format
+           update 
+          ];
+        });
 
-        dhall = {
-          path = ./dhall;
-          description = "Dhall development environment";
-        };
+      packages = rec {
+        default = dvt;
 
-        elixir = {
-          path = ./elixir;
-          description = "Elixir development environment";
-        };
-
-        elm = {
-          path = ./elm;
-          description = "Elm development environment";
-        };
-
-        gleam = {
-          path = ./gleam;
-          description = "Gleam development environment";
-        };
-
-        go = {
-          path = ./go;
-          description = "Go (Golang) development environment";
-        };
-
-        hashi = {
-          path = ./hashi;
-          description = "HashiCorp DevOps tools development environment";
-        };
-
-        haskell = {
-          path = ./haskell;
-          description = "Haskell development environment";
-        };
-
-        java = {
-          path = ./java;
-          description = "Java development environment";
-        };
-
-        kotlin = {
-          path = ./kotlin;
-          description = "Kotlin development environment";
-        };
-
-        nickel = {
-          path = ./nickel;
-          description = "Nickel development environment";
-        };
-
-        nim = {
-          path = ./nim;
-          description = "Nim development environment";
-        };
-
-        nix = {
-          path = ./nix;
-          description = "Nix development environment";
-        };
-
-        node = {
-          path = ./node;
-          description = "Node.js development environment";
-        };
-
-        opa = {
-          path = ./opa;
-          description = "Open Policy Agent development environment";
-        };
-
-        php = {
-          path = ./php;
-          description = "PHP development environment";
-        };
-
-        protobuf = {
-          path = ./protobuf;
-          description = "Protobuf development environment";
-        };
-
-        purescript = {
-          path = ./purescript;
-          description = "Purescript development environment";
-        };
-
-        python = {
-          path = ./python;
-          description = "Python development environment";
-        };
-
-        ruby = {
-          path = ./ruby;
-          description = "Ruby development environment";
-        };
-
-        rust = {
-          path = ./rust;
-          description = "Rust development environment";
-        };
-
-        rust-toolchain = {
-          path = ./rust-toolchain;
-          description = "Rust development environment with Rust version defined by a rust-toolchain.toml file";
-        };
-
-        scala = {
-          path = ./scala;
-          description = "Scala development environment";
-        };
-
-        zig = {
-          path = ./zig;
-          description = "Zig development environment";
-        };
-
-        latex = {
-            path = ./latex;
-            description = "A minimal customized latex template for documentation work";
-        };
-
-        typst = {
-            path = ./typst;
-            description = "Typst markup environment";
-        };
-
-        # Aliases
-        rt = rust-toolchain;
+        inherit dvt;
       };
-    } // flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        inherit (pkgs) mkShell writeScriptBin;
-        exec = pkg: "${pkgs.${pkg}}/bin/${pkg}";
-
-        format = writeScriptBin "format" ''
-          ${exec "nixpkgs-fmt"} **/*.nix
-        '';
-
-        dvt = writeScriptBin "dvt" ''
-          if [ -z $1 ]; then
-            echo "no template specified"
-            exit 1
-          fi
-
-          TEMPLATE=$1
-
-          ${exec "nix"} \
-            --experimental-features 'nix-command flakes' \
-            flake init \
-            --template \
-            "github:dragonginger10/nix-templates#''${TEMPLATE}"
-        '';
-
-        update = writeScriptBin "update" ''
-          for dir in `ls -d */`; do # Iterate through all the templates
-            (
-              cd $dir
-              ${exec "nix"} flake update # Update flake.lock
-              ${exec "direnv"} reload    # Make sure things work after the update
-            )
-          done
-        '';
-      in
-      {
-        devShells = {
-          default = mkShell {
-            packages = [ format update ];
-          };
-        };
-
-        packages = rec {
-          default = dvt;
-
-          inherit dvt;
-        };
-      });
+    };
 }
