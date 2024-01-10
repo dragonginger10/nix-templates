840c94a78589f8306b0c1343f304ad4b5f2c7a36
JP <dragonginger10@gmail.com>
2024-01-10 11:23:23 -0600

    update all and remove machnix

--- python/flake.nix
+++ python/flake.nix
@@ -4,28 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
     flake-utils.url = "github:numtide/flake-utils";
-    mach-nix.url = "github:/DavHau/mach-nix";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    mach-nix,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [
-        (self: super: {
-          machNix = mach-nix.defaultPackage.${system};
-          python = super.python311;
-        })
-      ];
-
-      pkgs = import nixpkgs {inherit overlays system;};
+      pkgs = import nixpkgs {inherit system;};
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs;
-          [python machNix virtualenv]
+          [python poetry]
           ++ (with pkgs.python311Packages; [pip]);
 
         shellHook = ''
