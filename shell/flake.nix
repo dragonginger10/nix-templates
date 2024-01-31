aa6d33814d49e579a277aa1705ed8b41eec51af5
JP <dragonginger10@gmail.com>
2024-01-31 10:49:18 -0600

    created a shell dev environment

---
+++ shell/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A nix flake shell dev environment";
+
+  inputs = {
+    nipkgs.url = "github:NixOS/nixpkgs/release-23.11";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    inherit (nixpkgs.lib) genAttrs;
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forEachSystem = f: genAttrs systems (system: f {
+      pkgs = import nixpkgs { inherit system; };
+    });
+  in
+  {
+    devShells = forEachSystem ({ pkgs }: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [ shellcheck ];
+      };
+    });
+  };
+}
