748b53c11c5ed5d09bbe1114a0ab0e72f844d010
JP <dragonginger10@gmail.com>
2024-03-01 12:40:59 -0600

    some issues with devshell sorted out

--- pyscript/flake.nix
+++ pyscript/flake.nix
@@ -1,49 +1,53 @@
 {
-  description = "A Nix-flake-based Python development environment";
+  description = "A Nix-flake-based Python script development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
   };
 
-
   outputs = {
     self,
     nixpkgs,
-  }:
-    let
-      supportedSystems = ["x86_64-linux" "aarch64-linux"];
-      forEachSystem =f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-      pname = "script";
-      version = "1.0";
-    in {
-      formatter = forEachSystem({ pkgs }: pkgs.alejandra);
-      packages = forEachSystem({pkgs}: rec {
-        default = errgen;
-        errgen = pkgs.stdenv.mkDerivation {
-          inherit pname version;
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux"];
+    forEachSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+    pname = "script";
+    version = "1.0";
+  in {
+    formatter = forEachSystem ({pkgs}: pkgs.alejandra);
+    packages = forEachSystem ({pkgs}: rec {
+      default = script;
+      script = pkgs.stdenv.mkDerivation {
+        inherit pname version;
 
-          propagatedBuildInputs = [
-            (pkgs.python311.withPackages (ps:
-              with ps; [
-                click
+        propagatedBuildInputs = [
+          (pkgs.python311.withPackages (ps:
+            with ps; [
+              rich
             ]))
-          ];
+        ];
 
-          dontUnpack = ":";
-          installPhase = "install -Dm755 ${./${pname}.py} $out/bin/${pname}";
-        };
-      });
+        dontUnpack = ":";
+        installPhase = "install -Dm755 ${./${pname}.py} $out/bin/${pname}";
+      };
+    });
 
-      devShells.default = forEachSystem({pkgs}: pkgs.mkShell {
+    devShells = forEachSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        # pulls from build inputs of packages
         packages = with pkgs;
-          [python311 virtualenv black isort]
-          ++ (with pkgs.python311Packages; [pip python-lsp-ruff]);
+          [virtualenv black isort]
+          ++ (with pkgs.python311Packages; [pip python-lsp-ruff])
+          ++ (self.packages.${pkgs.system}.default.propagatedBuildInputs);
 
         shellHook = ''
-          ${pkgs.python}/bin/python --version
+          ${pkgs.python311}/bin/python --version
         '';
-      });
-    };
+      };
+    });
+  };
 }
