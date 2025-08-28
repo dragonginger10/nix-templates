5dbe9ad2a18c8f2104ddb9b43cf4f7349a53317d
JP <dragonginger10@gmail.com>
2025-08-28 12:28:51 -0500

    alright lets flip to flake with flake-parts

---
+++ python/flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        devenv.shells.default = {
+          # https://devenv.sh/reference/options/
+          packages = with pkgs; [
+            just
+          ];
+
+          languages.python = {
+            enable = true;
+            uv.enable = true;
+          };
+        };
+      };
+    };
+}
